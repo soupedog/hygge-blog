17b7f03d1374a3c027fc6c4e5d802a567b796e40
Xavier.Wang <xavierpe@qq.com>
2022-08-14 22:00:08 +0800

    调整部署参数

--- script/app/initAppDockerfile.sh
+++ script/app/initAppDockerfile.sh
@@ -6,11 +6,11 @@ DB_PW=$3
 ES_HOST=$4
 
 cat > Dockerfile <<END_TEXT
-FROM openjdk:8-jdk-alpine
+FROM openjdk:17-jdk-alpine
 MAINTAINER Xavier xavierpe@qq.com
 COPY *.jar /hygge-blog-back-end.jar
 
-ENV JVM_OPTS="-Xmx640M -Xms640M -Xmn192M -XX:MaxMetaspaceSize=128M -XX:MetaspaceSize=128M -XX:+UseParallelGC -XX:+UseAdaptiveSizePolicy -XX:MaxGCPauseMillis=100 -XX:ErrorFile=/logFile/error/hs_err_pid%p.log   -Xloggc:/logFile/gc/gc.log -XX:HeapDumpPath=/logFile/dump -XX:+PrintGCDetails -XX:+PrintGCDateStamps -verbose:class -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintCommandLineFlags -XX:+PrintHeapAtGC"
+ENV JVM_OPTS="-Xmx640M -Xms640M"
 
 ENTRYPOINT ["/bin/sh","-c","java \$JVM_OPTS -jar -Ddatabase.url=${DB_HOST} -Ddatabase.userName=${DB_USER} -Ddatabase.password=${DB_PW} -DesHost=${ES_HOST} -Dspring.profiles.active=prod hygge-blog-back-end.jar"]
 END_TEXT
