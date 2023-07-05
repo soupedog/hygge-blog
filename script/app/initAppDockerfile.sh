3fc7f301f311008830f2eaa4504e67b3ae521275
Xavier.Wang <xavierpe@qq.com>
2023-07-05 20:58:06 +0800

    运维 调整 JVM 默认内存配置

--- script/app/initAppDockerfile.sh
+++ script/app/initAppDockerfile.sh
@@ -10,7 +10,7 @@ FROM openjdk:17-jdk-alpine
 MAINTAINER Xavier xavierpe@qq.com
 COPY *.jar /hygge-blog-back-end.jar
 
-ENV JVM_OPTS="-Xmx640M -Xms640M"
+ENV JVM_OPTS="-Xmx256M -Xms512M"
 
 ENTRYPOINT ["/bin/sh","-c","java \$JVM_OPTS -jar -Ddatabase.url=${DB_URL} -Ddatabase.userName=${DB_USER} -Ddatabase.password=${DB_PW} -DesUris=${ES_URIS} -Dspring.profiles.active=prod hygge-blog-back-end.jar"]
 END_TEXT
