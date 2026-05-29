3b64944ad4fcdc51108c08dca4177285b973265e
Xavier.Wang <xavierpe@qq.com>
2026-05-29 13:33:09 +0800

    后端 调整 Dockerfile 生成脚本

--- script/app/initAppDockerfile.sh
+++ script/app/initAppDockerfile.sh
@@ -1,27 +1,11 @@
 #!/usr/bin/env bash
 
-DB_URL=$1
-DB_USER=$2
-DB_PW=$3
-ES_URIS=$4
-ES_PW=$5
-FILE_PREFIX=$6
-IP_QUERY_KEY=$7
-
 cat > Dockerfile <<END_TEXT
 FROM amazoncorretto:17-alpine3.18-jdk
 
 COPY *.jar /hygge-blog-back-end.jar
 
-ENV database.url="${DB_URL}"
-ENV database.userName="${DB_USER}"
-ENV database.password="${DB_PW}"
-ENV esUris="${ES_URIS}"
-ENV esPassword="${ES_PW}"
-ENV file.link.prefix="${FILE_PREFIX}"
-ENV ipquery.key="${IP_QUERY_KEY}"
-
-ENV JVM_OPTS="-Xmx256M -Xms256M -Dspring.profiles.active=prod -Duser.language=zh -Duser.timezone=GMT+8"
+ENV JVM_OPTS="-Xms256m -Xmx256m -XX:MaxMetaspaceSize=100m -XX:+UseG1GC -Dspring.profiles.active=prod -Duser.language=zh -Duser.timezone=GMT+8"
 
 ENTRYPOINT ["/bin/sh","-c","java \$JVM_OPTS -jar hygge-blog-back-end.jar"]
 END_TEXT
