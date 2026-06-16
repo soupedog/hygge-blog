1146a93f9c3d7926f177afd117fb7c76c087caa8
Xavier.Wang <xavierpe@qq.com>
2026-06-17 00:42:08 +0800

    前端 优化代码

--- front-end/src/pages/component/HomeCategoryCollapse.tsx
+++ front-end/src/pages/component/HomeCategoryCollapse.tsx
@@ -1,4 +1,4 @@
-import {useContext} from 'react';
+import {useContext, useMemo} from 'react';
 import {Badge, Card, Collapse, type CollapseProps} from 'antd';
 import {HomeContext} from '../context/HomeContext.tsx';
 import {HomeKeywordType} from '../../enums/EnumKeeper.ts';
@@ -21,36 +21,38 @@ export default function HomeCategoryCollapse() {
         setActiveTap,
     } = useContext(HomeContext);
 
-    const items: CollapseProps['items'] = [
-        {
-            // 默认展开 default 面板
-            key: 'default',
-            label: '文章类别目录',
-            children:
-                <Card size={'small'}>
-                    {categoryList.map((item, index) => {
-                        if (item.articleCount < 1) {
-                            return null;
-                        }
-                        return (
-                            <Card.Grid key={'card_' + item.categoryName} className={clsx(['pointer'])} style={gridStyle}
-                                       onClick={() => {
-                                           setKeywordType(HomeKeywordType.POST);
-                                           searchPostSummaryByCid({cid: item.cid, currentPage: 1, pageSize: searchTabPageSize});
-                                           setActiveTap('搜索结果');
-                                       }}
-                            >
-                                <Badge.Ribbon key={'card_ribbon' + item.categoryName} style={{top: '-10px'}} text={item.articleCount} color='red'>
-                                    <div style={{padding: '0 15px 0 15px'}}>
-                                        {item.categoryName}
-                                    </div>
-                                </Badge.Ribbon>
-                            </Card.Grid>
-                        )
-                    })}
-                </Card>
-        }
-    ];
+    const items: CollapseProps['items'] = useMemo(() => {
+        return [
+            {
+                // 默认展开 default 面板
+                key: 'default',
+                label: '文章类别目录',
+                children:
+                    <Card size={'small'}>
+                        {categoryList.map((item, index) => {
+                            if (item.articleCount < 1) {
+                                return null;
+                            }
+                            return (
+                                <Card.Grid key={'card_' + item.categoryName} className={clsx(['pointer'])} style={gridStyle}
+                                           onClick={() => {
+                                               setKeywordType(HomeKeywordType.POST);
+                                               searchPostSummaryByCid({cid: item.cid, currentPage: 1, pageSize: searchTabPageSize});
+                                               setActiveTap('搜索结果');
+                                           }}
+                                >
+                                    <Badge.Ribbon key={'card_ribbon' + item.categoryName} style={{top: '-10px'}} text={item.articleCount} color='red'>
+                                        <div style={{padding: '0 15px 0 15px'}}>
+                                            {item.categoryName}
+                                        </div>
+                                    </Badge.Ribbon>
+                                </Card.Grid>
+                            )
+                        })}
+                    </Card>
+            }
+        ];
+    }, [searchTabPageSize, JSON.stringify(categoryList)]);
 
     return (
         <Collapse activeKey={[categoryCollapsed ? 'collapsed' : 'default']} items={items} style={{backgroundColor: '#FFF'}}/>
--- front-end/src/pages/component/HomeTabs.tsx
+++ front-end/src/pages/component/HomeTabs.tsx
@@ -1,7 +1,6 @@
-import {useContext} from 'react';
+import {useContext, useMemo} from 'react';
 import {Badge, Tabs, type TabsProps} from 'antd';
 import {HomeContext} from '../context/HomeContext.tsx';
-import type {TopicOverviewInfo} from '../../util/ApiClient.ts';
 
 export default function HomeTabs() {
     const {
@@ -12,34 +11,20 @@ export default function HomeTabs() {
         announcementInfoList,
     } = useContext(HomeContext);
 
-    return (
-        <Tabs activeKey={activeTap} size={'large'} type={'card'} style={{marginTop: '1rem', padding: '1rem', borderRadius: '0.5rem', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', backgroundColor: '#FFF'}}
-            // @ts-ignore
-              items={buildItems(topicOverviewInfoList)}
-              onChange={(key) => {
-                  setActiveTap(key);
-              }}
-        />
-    );
+    const items: TabsProps['items'] = useMemo(() => {
+        const dynamicTabs = topicOverviewInfoList.map((item) => ({
+            key: item.topicInfo.tid,
+            label: (
+                <>
+                    {item.topicInfo.topicName}
+                    <Badge count={item.totalCount} overflowCount={9999} offset={[10, -20]}/>
+                </>
+            ),
+            children: <div>{item.topicInfo.topicName} 内容：</div>,
+        }));
 
-    function buildItems(list: Array<TopicOverviewInfo>) {
-        const items = new Array<TabsProps>();
-        list.map((item) => {
-            items.push(
-                {
-                    key: item.topicInfo.tid,
-                    label: (
-                        <>
-                            {item.topicInfo.topicName}
-                            <Badge count={item.totalCount} overflowCount={9999} offset={[10, -20]}/>
-                        </>
-                    ),
-                    children: <div>{item.topicInfo.topicName} 内容：</div>,
-                } as TabsProps
-            );
-        });
-
-        items.push(
+        return [
+            ...dynamicTabs,
             {
                 key: '句子收藏',
                 label: (
@@ -48,39 +33,41 @@ export default function HomeTabs() {
                         <Badge count={quoteInfo.totalCount} overflowCount={9999} offset={[10, -20]}/>
                     </>
                 ),
-                children: <div></div>,
-            } as TabsProps
-        );
-
-        items.push(
+                children: <div>句子收藏内容</div>,
+            },
             {
                 key: '搜索结果',
                 label: (
                     <>
                         搜索结果
-                        <Badge
-                            count={searchResultTotalCount}
-                            overflowCount={9999}
-                            offset={[10, -20]}/>
+                        <Badge count={searchResultTotalCount} overflowCount={9999} offset={[10, -20]}/>
                     </>
                 ),
-                children: <div></div>,
-            } as TabsProps
-        );
-
-        items.push(
+                children: <div>搜索结果内容</div>,
+            },
             {
                 key: '公告',
                 label: (
                     <>
                         公告
-                        <Badge count={announcementInfoList.length} overflowCount={9999}
-                               offset={[10, -20]}/>
+                        <Badge count={announcementInfoList.length} overflowCount={9999} offset={[10, -20]}/>
                     </>
                 ),
-                children: <div></div>,
-            } as TabsProps
-        );
-        return items;
-    }
+                children: <div>公告内容</div>,
+            },
+        ];
+    }, [topicOverviewInfoList, quoteInfo.totalCount, searchResultTotalCount, announcementInfoList.length]);
+
+    return (
+        <Tabs
+            activeKey={activeTap}
+            size={'large'}
+            type={'card'}
+            style={{marginTop: '1rem', padding: '1rem', borderRadius: '0.5rem', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', backgroundColor: '#FFF'}}
+            items={items}
+            onChange={(key) => {
+                setActiveTap(key);
+            }}
+        />
+    );
 }
