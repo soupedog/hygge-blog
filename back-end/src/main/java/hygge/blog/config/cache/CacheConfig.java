be3fae4c80f826d0e6b969cdfc83293ede9a4888
Xavier.Wang <xavierpe@qq.com>
2026-06-22 02:22:33 +0800

    后端 查询缓存切换到 CaffeineCacheManager 提供基本的过期机制

--- back-end/src/main/java/hygge/blog/config/cache/CacheConfig.java
+++ back-end/src/main/java/hygge/blog/config/cache/CacheConfig.java
@@ -1,9 +1,13 @@
 package hygge.blog.config.cache;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Xavier
@@ -16,6 +20,12 @@ public class CacheConfig {
      */
     @Bean
     public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        // 设置默认的缓存策略：写入后10分钟过期，最大容量1000
+        Caffeine<Object, Object> caffeine = Caffeine.newBuilder()
+                .expireAfterWrite(1, TimeUnit.HOURS)
+                .maximumSize(1000);
+        cacheManager.setCaffeine(caffeine);
         return new ConcurrentMapCacheManager();
     }
 }
