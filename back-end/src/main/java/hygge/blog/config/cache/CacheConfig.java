0e9774bf9cd3e42f8abbc68790cf72af9a4ee30e
Xavier.Wang <xavierpe@qq.com>
2026-07-01 23:20:22 +0800

    后端 修正 CacheManager 配置错误问题

--- back-end/src/main/java/hygge/blog/config/cache/CacheConfig.java
+++ back-end/src/main/java/hygge/blog/config/cache/CacheConfig.java
@@ -3,7 +3,6 @@ package hygge.blog.config.cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,6 +25,6 @@ public class CacheConfig {
                 .expireAfterWrite(1, TimeUnit.HOURS)
                 .maximumSize(1000);
         cacheManager.setCaffeine(caffeine);
-        return new ConcurrentMapCacheManager();
+        return cacheManager;
     }
 }
